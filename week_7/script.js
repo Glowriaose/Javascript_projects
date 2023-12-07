49093cea3ae812e9f2a608306d4e01c6c91731d7
glowria <osemudiamegloria51@gmail.com>
2023-12-07 09:08:08 +0100

    A program that generates a random number between 1 and 100

--- week_7/script.js
+++ week_7/script.js
@@ -1,9 +1,25 @@
-function factorialCal() {
-  let factorial = 1;
-  for (let n = 1; n <= 6; n++);
-  console.log(n);
-  factorial = factorial * n;
-
-  result = factorialCal();
-  return result;
+// function factorialCal() {
+//   let factorial = 1;
+//   for (let n = 1; n <= 6; n++);
+//   {
+//     console.log(n);
+//     factorial = factorial * n;
+//   }
+//   result = factorialCal();
+//   return result;
+// }
+function guessingGame(guess) {
+  let randomNum = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNum);
+  if (guess < randomNum) {
+    console.log(`the number ${guess} is too low.`);
+  }
+  if (guess > randomNum) {
+    console.log(`the number ${guess} is too high.`);
+  } else if (guess === randomNum) {
+    console.log(`😊 the number guessed,${guess} is correct`);
+  } else {
+    return '😒 error!!!';
+  }
 }
+guessingGame(88);
